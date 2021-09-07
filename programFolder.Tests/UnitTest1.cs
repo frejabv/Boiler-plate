4964ecf0cbfc50ee99b40e6eb5f55cb39eda0b64
frev <frev@itu.dk>
2021-09-07 13:16:54 +0200

    Implement user input

--- programFolder.Tests/UnitTest1.cs
+++ programFolder.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace programFolder.Tests
 {
@@ -40,6 +41,26 @@ namespace programFolder.Tests
             var result = Program.IsLeapYear(400);
             //assert
             Assert.True(result);
+        }
+
+        [Fact]
+        public void printsYayOnLeapYear() 
+        {
+            //arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var input = new StringReader("1600");
+            Console.SetIn(input);
+            //act
+            Program.Main(new string[0]);
+            var output = writer.GetStringBuilder().ToString();
+
+
+            //assert
+            Console.WriteLine(output);
+            //todo pls fix
+            Assert.Equal("yay\n",output); //ordn det der med new line som han også havde i test til forelæsningen
+            //Assert.True(result);
         }
     }
 }
--- programFolder/Program.cs
+++ programFolder/Program.cs
@@ -6,7 +6,17 @@ namespace programFolder
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please type a year:");
+            int year = receiveInput();
+            bool result = IsLeapYear(year);
+            if(result) 
+            {
+                Console.WriteLine("yay");
+            } 
+            else 
+            {
+                Console.WriteLine("nay");
+            }
         }
 
         public static bool IsLeapYear(int year) 
@@ -27,6 +37,22 @@ namespace programFolder
             }
 
             return isLeapYear;
+        }
+
+        public static int receiveInput() 
+        {
+            int year;
+            try 
+            {
+                year = int.Parse(Console.ReadLine());
+            } 
+            catch (Exception e) 
+            {
+                //e.GetType();
+                Console.WriteLine("Please only type numbers:");
+                year = receiveInput();
+            }
+            return year;
         }
     }
 }
