411611a2ce5c1c72130d1814892a127647577160
frev <frev@itu.dk>
2021-09-07 13:45:24 +0200

    Add tests

--- programFolder.Tests/UnitTest1.cs
+++ programFolder.Tests/UnitTest1.cs
@@ -6,6 +6,18 @@ namespace programFolder.Tests
 {
     public class UnitTest1
     {
+        [Theory]
+        [InlineData(4,true)]
+        [InlineData(8,true)]
+        [InlineData(200,false)]
+        [InlineData(1600,true)]
+        [InlineData(1700,false)]
+        public void generalLeapYearValidity(int year, bool value) {
+            var result = Program.IsLeapYear(year);
+            Assert.Equal(value, result);
+        }
+
+
         [Fact]
         public void divisibleBy4True()
         {
@@ -27,10 +39,8 @@ namespace programFolder.Tests
             //arrange
             //act
             var result1 = Program.IsLeapYear(200);
-            //var result2 = Program.IsLeapYear();
             //assert
             Assert.False(result1);
-            //Assert.False(result2);
         }
 
         [Fact]
@@ -51,16 +61,39 @@ namespace programFolder.Tests
             Console.SetOut(writer);
             var input = new StringReader("1600");
             Console.SetIn(input);
+
             //act
             Program.Main(new string[0]);
             var output = writer.GetStringBuilder().ToString();
 
-
             //assert
             Console.WriteLine(output);
             //todo pls fix
-            Assert.Equal("yay\n",output); //ordn det der med new line som han også havde i test til forelæsningen
-            //Assert.True(result);
+            var array = output.Split("\n");
+            output = array[1];
+            Assert.Equal("yay",output); //ordn det der med new line som han også havde i test til forelæsningen
+        }
+
+        [Theory]
+        [InlineData("1600", "yay")]
+        [InlineData("1705", "nay")]
+        public void printsYayOrNay(string inputYear, string expectedPrint) 
+        {
+            //arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var input = new StringReader(inputYear);
+            Console.SetIn(input);
+
+            //act
+            Program.Main(new string[0]);
+            var output = writer.GetStringBuilder().ToString();
+
+            //assert
+            Console.WriteLine(output);
+            var array = output.Split("\n");
+            output = array[1];
+            Assert.Equal(expectedPrint,output); 
         }
     }
 }
