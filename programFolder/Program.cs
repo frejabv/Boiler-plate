bdd479c9a5897484ce3b562315782e104a091302
frev <frev@itu.dk>
2021-09-07 15:02:41 +0200

    Modify if-statemnets

--- programFolder/Program.cs
+++ programFolder/Program.cs
@@ -24,19 +24,18 @@ namespace programFolder
         {
             bool isLeapYear = false;
 
-
             if(year%4 == 0) 
             {
                 isLeapYear = true;
-            }
-            if(year%100 == 0) 
-            {
-                isLeapYear = false;
-            }
-            if(year%400 == 0) 
-            {
-                isLeapYear = true;
-            }
+                if(year%100 == 0) 
+                {
+                    isLeapYear = false;
+                    if(year%400 == 0) 
+                    {
+                        isLeapYear = true;
+                    }
+                } 
+            } 
 
             return isLeapYear;
         }
