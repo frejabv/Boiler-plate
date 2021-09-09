3532f41d7a63378c9c454726786732325965e78e
frejabv <90038232+frejabv@users.noreply.github.com>
2021-09-09 13:42:57 +0200

    Leap year (#2)
    Implement leap year algorithm

--- programFolder.Tests/UnitTest1.cs
+++ programFolder.Tests/UnitTest1.cs
@@ -1,14 +1,144 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace programFolder.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-
+        [Theory]
+        [InlineData(4,true)]
+        [InlineData(8,true)]
+        [InlineData(7,false)]
+        [InlineData(200,false)]
+        [InlineData(1600,true)]
+        [InlineData(1700,false)]
+        public void generalLeapYearValidity(int year, bool value) {
+            var result = Program.IsLeapYear(year);
+            Assert.Equal(value, result);
         }
+
+
+        [Fact]
+        public void divisibleBy4True()
+        {
+            //arrange
+            //I want to get access to the output?
+
+            //act
+            //I run the method with a number divisible by four and save the result
+            var result = Program.IsLeapYear(8);
+
+            //assert
+            //I assert that the input is divisible by four and the method gives the correct output
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void divisibleBy4And100()
+        {
+            //arrange
+            //act
+            var result1 = Program.IsLeapYear(200);
+            //assert
+            Assert.False(result1);
+        }
+
+        [Fact]
+        public void divisibleBy400()
+        {
+            //arrange
+            //act
+            var result = Program.IsLeapYear(400);
+            //assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void printsYayOnLeapYear() 
+        {
+            //arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var input = new StringReader("1600");
+            Console.SetIn(input);
+
+            //act
+            Program.Main(new string[0]);
+            var output = writer.GetStringBuilder().ToString();
+
+            //assert
+            Console.WriteLine(output);
+            //todo pls fix
+            //ordn det der med new line som han også havde i test til forelæsningen
+            var array = output.Split("\n");
+            output = array[1];
+            Assert.Equal("yay",output); 
+        }
+
+        [Theory]
+        [InlineData("1600", "yay")]
+        [InlineData("1705", "nay")]
+        public void printsYayOrNay(string inputYear, string expectedPrint) 
+        {
+            //arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var input = new StringReader(inputYear);
+            Console.SetIn(input);
+
+            //act
+            Program.Main(new string[0]);
+            var output = writer.GetStringBuilder().ToString();
+
+            //assert
+            Console.WriteLine(output);
+            var array = output.Split("\n");
+            output = array[1];
+            Assert.Equal(expectedPrint,output); 
+        }
+
+        //somehow stop the method...
+        /*[Fact]
+        public void onlyAcceptsNumbers() 
+        {
+            //arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var input = new StringReader("hewwo");
+            Console.SetIn(input);
+
+            //act
+            Program.Main(new string[0]);
+            var output = writer.GetStringBuilder().ToString();
+
+            //assert
+            Console.WriteLine(output);
+            //todo pls fix
+            var array = output.Split("\n");
+            output = array[1];
+            Assert.Equal("Please only type numbers",output);
+        }*/
+
+        /*[Fact]
+        public void onlyAcceptsYearsPast1582() 
+        {
+            //arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var input = new StringReader("700");
+            Console.SetIn(input);
+
+            //act
+            Program.Main(new string[0]);
+            var output = writer.GetStringBuilder().ToString();
+
+            //assert
+            Console.WriteLine(output);
+            //todo pls fix
+            var array = output.Split("\n");
+            output = array[1];
+            Assert.Equal("Leap years only really count from 1582 and up, please try again",output);
+        }*/
     }
 }
--- programFolder/Program.cs
+++ programFolder/Program.cs
@@ -2,11 +2,74 @@
 
 namespace programFolder
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please type a year:");
+            int year = receiveInput();
+            bool result = IsLeapYear(year);
+
+            if(result) 
+            {
+                Console.WriteLine("yay");
+            } 
+            else 
+            {
+                Console.WriteLine("nay");
+            }
+        }
+
+        public static bool IsLeapYear(int year) 
+        {
+            bool isLeapYear = false;
+
+            if(year%4 == 0) 
+            {
+                isLeapYear = true;
+                if(year%100 == 0) 
+                {
+                    isLeapYear = false;
+                    if(year%400 == 0) 
+                    {
+                        isLeapYear = true;
+                    }
+                } 
+            } 
+
+            return isLeapYear;
+        }
+
+        public static int receiveInput() {
+            bool inputNotReceived = true;
+            int year = 0;
+
+            while(inputNotReceived) {
+                try {
+                    year = int.Parse(Console.ReadLine());
+                    if(year < 1582) {
+                        Console.WriteLine("Leap years only really count from 1582 and up, please try again");
+                        inputNotReceived = true;
+                    } else {
+                        inputNotReceived = false;
+                    }
+                } catch(Exception e) {
+                    Console.WriteLine("Please only type numbers");
+                }
+            }
+
+            /*int year;
+            try {
+                year = int.Parse(Console.ReadLine());
+            } catch(Exception e) {
+                Console.WriteLine("Please only type numbers");
+                year = receiveInput();
+            }
+            if(year < 1582) {
+                Console.WriteLine("Leap years only really count from 1582 and up, please try again");
+                year = receiveInput();
+            }*/
+            return year;
         }
     }
 }
